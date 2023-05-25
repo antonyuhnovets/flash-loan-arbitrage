3c761ee6aaef2537f9bb20f08df8b3982b72e2b3
anton <anton@localhost.localdomain>
2023-05-25 16:59:01 +0300

    dev-02: add constructor

--- contracts/aave-contract.sol
+++ contracts/aave-contract.sol
@@ -2,3 +2,15 @@
 pragma solidity 0.8.19;
 
 import "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+
+
+contract ArbitrargeFlashLoan is FlashLoanSimpleReceiverBase {
+    address payable owner;
+
+    constructor(address _addressProvider)
+        FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider))
+    {
+        owner = payable(msg.sender);
+    }
+}
