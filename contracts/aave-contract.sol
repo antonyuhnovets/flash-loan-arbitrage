8ca851e15118abaa5ed048dec235aaa38465789a
anton <anton@localhost.localdomain>
2023-05-26 12:55:28 +0300

    dev-03: implent flash loan request

--- contracts/aave-contract.sol
+++ contracts/aave-contract.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
 import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 
 
 contract ArbitrargeFlashLoan is FlashLoanSimpleReceiverBase {
@@ -13,4 +14,22 @@ contract ArbitrargeFlashLoan is FlashLoanSimpleReceiverBase {
     {
         owner = payable(msg.sender);
     }
+
+    function fn_RequestFlashLoan(address _token, uint256 _amount) public {
+        address receiverAddress = address(this);
+        address asset = _token;
+        uint256 amount = _amount;
+        bytes memory params = "";
+        uint16 referralCode = 0;
+
+        // Take the loan
+        POOL.flashLoanSimple(
+            receiverAddress,
+            asset,
+            amount,
+            params,
+            referralCode
+        );
+    }
+
 }
